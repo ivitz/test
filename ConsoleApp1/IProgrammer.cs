aa293b677cecf775ef0d934b182ba1c11b5d0091
ivitz <victivitz@gmail.com>
2019-06-20 17:06:44 +0300

    new interface added

---
+++ ConsoleApp1/IProgrammer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    interface IProgrammer
+    {
+        string Language { get; set; }
+        int ExperienceInYears { get; set; }
+        float Salary { get; set; }
+    }
+}
